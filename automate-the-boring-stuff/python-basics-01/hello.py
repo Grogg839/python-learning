f6411d013d772d7e9262b8ad3f4d0d9f893d98a1
Grogg <george.priest2005@yahoo.com>
2026-09-02 20:06:05 +0100

    Changed format of directory and started the book

---
+++ automate-the-boring-stuff/python-basics-01/hello.py
@@ -0,0 +1,11 @@
+# This program should say hello and then ask for my name
+
+print('Hello, world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('How old are you?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
