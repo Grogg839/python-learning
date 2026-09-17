a60e1ac451d9ed2561c1b852121f0af3fa54e2b8
Grogg <george.priest2005@yahoo.com>
2026-09-17 18:47:37 +0100

    Finished Chapter 2

---
+++ automate-the-boring-stuff/flow-control-02/practice-q13.py
@@ -0,0 +1,8 @@
+for i in range(1, 11, 1):
+   print(i)
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
---
+++ automate-the-boring-stuff/flow-control-02/practice-q9.py
@@ -0,0 +1,9 @@
+print('Pick a number, 1, 2 or 3')
+
+spam = int(input())
+if spam == 1:
+    print('Hello')
+if spam == 2:
+    print('Howdy')
+if spam == 3:
+    print('Greetings!')
